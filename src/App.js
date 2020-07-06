7fc431c4cf57b6c3af47d8b5dd451c270a05a406
Carlos Cesar Valle <carloscesarvalle@Carloss-iMac.local>
2020-07-06 00:30:29 -0400

    iteration2

--- src/App.js
+++ src/App.js
@@ -1,26 +1,55 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import 'bulma/css/bulma.css';
 import './App.css';
+import foods from './foods.json';
+import FoodBox from './components/FoodBox.js';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    food: [...foods],
+  };
+
+  displayFood = () => {
+    let displayFoodNow = this.state.food.map((eachElement) => {
+      return (
+        <FoodBox
+          key={eachElement.name}
+          name={eachElement.name}
+          calories={eachElement.calories}
+          image={eachElement.image}
+          quantity={eachElement.quantity}
+        />
+      );
+    });
+    return displayFoodNow;
+  };
+
+  addNewFood = (name, calories, image, quantity) => {
+    let newFood = {
+      name: name,
+      calories: calories,
+      image: image,
+      quantity: quantity,
+    };
+
+    let foodCopy = [...this.state.food];
+
+    foodCopy.push(newFood);
+
+    this.setState((food = foodCopy));
+  };
+
+  displayForm = () => {};
+
+  render() {
+    return (
+      <div>
+        <button onClick={this.displayForm}>Add new food</button>
+        {this.displayFood()}
+        />
+      </div>
+    );
+  }
 }
 
 export default App;
